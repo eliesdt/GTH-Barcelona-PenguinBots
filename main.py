4acdc9a65985ff4d4326c21387a2bf42d537d9b9
eliesdt <39184093+eliesdt@users.noreply.github.com>
2019-09-28 15:37:20 +0200

    final update

--- main.py
+++ main.py
@@ -4,32 +4,165 @@ import os
 import json
 import requests
 from bs4 import BeautifulSoup as Soup
+import spotipy.util as util
+import spotipy
+import spotipy.oauth2 as oauth2
+from dateutil.parser import parse
+import datetime
 
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-#app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
 port = int(os.environ.get('PORT', 8888))
 
-SONGKICK_API_KEY = 'gULl2n89adTbdK7G'
+client_id = 'CLIENT_ID'
+client_secret = 'CLIENT_SECRET'
+redirect_uri = 'http://localhost:8888/callback'
+
+scope = 'user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played user-top-read playlist-modify-public'
+
+SONGKICK_API_KEY = 'SONGKICK_API_KEY'
+
+artists = []
+gig_dates = []
+gig_uris = []
+tracks = []
+tracks_name = []
+tracks_image = []
+
+location = ''
+min_date = ''
+max_date = ''
+
+CITY = ''
+
+curr_artist = 100
+curr_round = 100
+ready = True
+
+player_url = "https://api.spotify.com/v1/me/player"
+
+prev_url = ''
+
+credentials = oauth2.SpotifyClientCredentials(client_id = "CLIENT_ID", client_secret = "CLIENT_SECRET")
+token = credentials.get_access_token()
+sp = spotipy.Spotify(auth=token)
 
 @app.route("/callback", methods=['GET'])
 def callback():
-    print(request.args.get('code'))
+    global sp
+    credentials = spotipy.oauth2.SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        scope=scope,
+        state=None,
+        cache_path=None,
+        proxies=None)
+
+    code = request.args.get('code')
+    global token
+    token = credentials.get_access_token(code)
+    token = token['access_token']
+
     return 'ok'
 
-#localhost:8888/get_artists?location=france&min_date=2019-10-03&max_date=2019-10-05
-@app.route("/get_artists", methods=['GET'])
-def get_artists():
-    artists = []
-    gig_dates = []
-    gig_uris = []
+@app.route("/play", methods=['GET'])
+def play():
+    print("PLAAY")
+    print(curr_artist)
+    print(len(tracks))
+    artist_index = curr_artist % len(tracks)
+    update_player(artist_index)
+    info = get_info_artist(artist_index)
+    return info
 
-    LOCATION = request.args.get('location')
-    MIN_DATE = request.args.get('min_date')
-    MAX_DATE = request.args.get('max_date')
+@app.route("/pause", methods=['GET'])
+def pause():
+    print("PAUSEE")
+    return 'pause'
 
+@app.route("/previous", methods=['GET'])
+def previous():
+    global curr_artist
+    curr_artist -= 1
+    artist_index = (curr_artist) % len(tracks)
+    update_player(artist_index)
+    info = get_info_artist(artist_index)
+    return info
+
+@app.route("/next", methods=['GET'])
+def next():
+    global curr_artist
+    curr_artist += 1
+    artist_index = (curr_artist) % len(tracks)
+    update_player(artist_index)
+    info = get_info_artist(artist_index)
+    return info
+
+@app.route("/create_playlist", methods=['GET'])
+def create_playlist():
+    return 'create playlist'
+
+@app.route("/kiwi_detected", methods=['GET'])
+def kiwi_detected():
+    print('kiwi_detected')
+    url = request.args.get('url')
+    url = url.split('/')
+
+    global location
+    global min_date
+    global max_date
+
+    global ready
+    global prev_url
+
+    global artists
+    global gig_dates
+    global gig_uris
+    global tracks
+    global tracks_name
+    global tracks_image
+
+    if is_date(url[-1]) == True and is_date(url[-2]) == True and url[-3] != '-' and ready == True and url != prev_url:
+        ready == False
+        artists = []
+        gig_dates = []
+        gig_uris = []
+        tracks = []
+        tracks_name = []
+        tracks_image = []
+        
+        max_date = url[-1]
+        min_date = url[-2]
+        location = url[-3]
+        print(location,min_date,max_date)
+        get_tracks()
+
+        artist_index = curr_artist % len(tracks)
+        update_player(artist_index)
+        info = get_info_artist(artist_index)
+        ready = True
+        prev_url = url
+        return info
+    else:
+        return 'kiwi_detected'
+
+def get_info_artist(artist_index):
+    info = str(artists[artist_index])+'$'+str(tracks_name[artist_index][0])+'$'+str(tracks_image[artist_index][0])+'$'+str(gig_dates[artist_index])+'$'+str(gig_uris[artist_index])+'$'+str(CITY)
+    return info
+
+def is_date(string, fuzzy=False):
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
+
+def get_artists(LOCATION, MIN_DATE, MAX_DATE):
     LOCATION = LOCATION.split('-')
+    global CITY
     CITY = LOCATION[0]
     COUNTRY = LOCATION[-1]
 
@@ -50,7 +183,9 @@ def get_artists():
     for concert in js['resultsPage']['results']['event']:
         for artist in concert['performance']:
             gig_uris.append(concert['uri'])
-            gig_dates.append(concert['start']['date'])
+            date = concert['start']['date']
+            date = datetime.datetime.strptime(date, "%Y-%m-%d")
+            gig_dates.append(datetime.date.strftime(date, "%d %B"))
             artists.append(artist['displayName'])
 
     for i in reversed(range(len(artists))):
@@ -71,11 +206,46 @@ def get_artists():
             del gig_dates[i]
             del gig_uris[i]
 
-    response = ''
-    for i in range(len(artists)):
-        response = response + '{} will perfom on {}, more info: {}'.format(artists[i],gig_dates[i],gig_uris[i])
+def get_tracks():
+    print('Getting tracks...')
+    get_artists(location, min_date, max_date)
 
-    return f"{response}"
+    artists_id = []
+    global token
+    sp = spotipy.Spotify(auth=token)
+
+    for artist in artists:
+        top_tracks = []
+        top_tracks_names = []
+        top_tracks_images = []
+        artist_searched = sp.search(artist, limit=10, offset=0, type='artist', market=None)
+        artist_id = artist_searched['artists']['items'][0]['id']
+        artists_id.append(artist_id)
+        for track in sp.artist_top_tracks(artist_id)['tracks']:
+            print('track:',track)
+            track_id = track['uri']
+            track_name = track['name']
+            track_image = track['album']['images'][1]['url']
+            top_tracks.append(track_id)
+            top_tracks_names.append(track_name)
+            top_tracks_images.append(track_image)
+            print(track_id)
+        tracks.append(top_tracks)
+        tracks_name.append(top_tracks_names)
+        tracks_image.append(top_tracks_images)
+
+    print(tracks)
+    print(tracks_name)
+    print(tracks_image)
+
+    artist_index = curr_artist % len(tracks)
+    update_player(artist_index)
+
+def update_player(artist_index):
+    authorization = {'Authorization': 'Bearer ' + token}
+    params = {'uris': [tracks[artist_index][0]]}
+    response = requests.put(player_url + '/play', headers=authorization, data=json.dumps(params))
+    print('playback:',response.content)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
